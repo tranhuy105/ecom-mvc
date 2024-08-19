314e0a98fcdb67f0eeaa5d9c7ee8704c8251e88b
tranhuy105 <imhuy105@gmail.com>
2024-08-19 17:09:59 +0700

    first commit

---
+++ WebParent/EcomBackend/src/test/java/com/tranhuy105/admin/EcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tranhuy105.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
