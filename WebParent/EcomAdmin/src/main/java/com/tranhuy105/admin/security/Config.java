4671b4468416c7154b04d7f1acad388ca3c06ec2
tranhuy105 <imhuy105@gmail.com>
2024-08-20 23:14:28 +0700

    add logout

--- WebParent/EcomAdmin/src/main/java/com/tranhuy105/admin/security/Config.java
+++ WebParent/EcomAdmin/src/main/java/com/tranhuy105/admin/security/Config.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,7 +35,7 @@ public class Config {
                 .loginPage("/login")
                 .usernameParameter("email")
                 .permitAll()
-        );
+        ).logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }
--- WebParent/EcomAdmin/src/main/java/com/tranhuy105/admin/security/MyUserDetail.java
+++ WebParent/EcomAdmin/src/main/java/com/tranhuy105/admin/security/MyUserDetail.java
@@ -37,4 +37,8 @@ public class MyUserDetail implements UserDetails {
     public boolean isEnabled() {
         return user.isEnabled();
     }
+
+    public String getFullName(){
+        return user.getFirstName() + " " + user.getLastName();
+    }
 }
