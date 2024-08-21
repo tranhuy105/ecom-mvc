c165817492ceec16d095e19d7c8002a4e9d2a0f5
tranhuy105 <imhuy105@gmail.com>
2024-08-21 16:37:40 +0700

    add category model & schema

---
+++ Common/src/main/java/com/tranhuy105/common/entity/Category.java
@@ -0,0 +1,55 @@
+package com.tranhuy105.common.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(
+        name = "categories",
+        indexes = {
+                @Index(name = "idx_category_name", columnList = "name"),
+                @Index(name = "idx_category_alias", columnList = "alias"),
+                @Index(name = "idx_category_parent_id", columnList = "parent_id")
+        }
+)
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 128, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 64, nullable = false, unique = true)
+    private String alias;
+
+    @Column(length = 128, nullable = false)
+    private String image;
+
+    private boolean enabled = true;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Category> children = new HashSet<>();
+
+    public void addChild(Category child) {
+        child.setParent(this);
+        this.children.add(child);
+    }
+
+    public void removeChild(Category child) {
+        child.setParent(null);
+        this.children.remove(child);
+    }
+}
